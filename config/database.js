0da2a0cbd66425a3eb9aa4d870105b9b0f729f5c
Albert Sharapov <elistaphilly@gmail.com>
2022-10-30 20:31:36 -0500

    update images

--- config/database.js
+++ config/database.js
@@ -6,9 +6,9 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USER', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0000'),
+      database: env('POSTGRES_DB', 'strapi'),
+      user: env('POSTGRES_USER', 'postgres'),
+      password: env('POSTGRES_PASSWORD', '0000'),
       ssl: env('DATABASE_SSL', false),
     },
     debug: false,
