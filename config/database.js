cdabc2a858a49635684f6543b707ff6d5d65f408
Albert Sharapov <elistaphilly@gmail.com>
2022-10-31 03:35:44 -0500

    db host change

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '0.0.0.0'),
+      host: env('DATABASE_HOST', 'postgres'),
       port: env('DATABASE_PORT', 5432),
       database: env('POSTGRES_DB', 'strapi'),
       user: env('POSTGRES_USER', 'postgres'),
