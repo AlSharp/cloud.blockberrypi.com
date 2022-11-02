7b1c2241fce339c29b27af2183510d81ab651bef
Albert Sharapov <elistaphilly@gmail.com>
2022-11-02 00:56:12 -0500

    album api

---
+++ config/plugins.js
@@ -0,0 +1,13 @@
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      breakpoints: {
+        xlarge: 1920,
+        large: 1280,
+        medium: 760,
+        small: 500,
+        xsmall: 64
+      },
+    },
+  },
+});
